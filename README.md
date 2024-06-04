b7251b8c6b5489cee88f5021cccbcfe46d64d9a4
Emmanuel Jatto <emmanueljatto33@gmail.com>
2024-06-03 23:05:25 -0400

    made changes to script.js, added a image folder along with a screenshot, and made changes to readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # task-board
+The goal of this project was to create a task board so that a user could add project tasks, and manage their state of progress and track their projects progress.  The page needed to be able take user input from a modal form, store the inputs in local storage, place the user input in the appropriate sections as a card, and have the user input cards be draggable.  
+
+This application is a webpage of a task board. 
+
+There were many challenges that I faced while creating this task board.  One challenge was using day.js to format the color of the card.  I solved this challenge by using the isAfter and isSame command, to compare the due date and the current date. 
+
+![Task Board Screenshot](./assets/images/taskboard-screenshot.jpg)
+
+[Task ]
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const toDoListDiv = $('#todo-cards');
+const inProgressCardsDiv = $('#in-progress-cards');
+const doneCardsDiv = $('#done-cards');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -9,21 +12,28 @@ function generateTaskId() {
 }
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    for(let i = 0; i < tasks.length; i++) {
+
+    // Creates the task cards and adds bootstrap to the classes for easy formatting
     const taskCard = $('<div>');
     taskCard.addClass('card task-card draggable my-3');
     taskCard.attr('data-task-id', task.id);
-    const taskCardHeader = $('<div>').addClass('card-header h4').text(task[i].title);
+    const taskCardHeader = $('<div>').addClass('card-header h4').text(task.title);
     const taskCardBody = $('<div>').addClass('card-body');
-    const taskCardText = $('<p>').addClass('card-text').text(task[i].description);
+    const taskCardText = $('<p>').addClass('card-text').text(task.description);
+    const taskCardDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const taskCardButton = $('<button>').addClass("btn btn-danger").text('Delete');
+    taskCardButton.attr('id', task.id);
+    taskCardButton.on('click', handleDeleteTask);
+
     taskCard.append(taskCardHeader);
     taskCard.append(taskCardBody);
     taskCardBody.append(taskCardText);
-}
-
-    // append to proper div
+    taskCardBody.append(taskCardDate);
+    taskCardBody.append(taskCardButton);
+    
     if (task.status === 'to-do') {
         $('#todo-cards').append(taskCard);
+        taskCard.addClass('draggable');
     }
     else if(task.status === 'in-progress') {
         $('#in-progress-cards').append(taskCard);
@@ -32,13 +42,44 @@ function createTaskCard(task) {
         $('#done-cards').append(taskCard);
     }
 
+    // Uses dayjs to determine color of card
+    const currentDate = dayjs();
+    const dueDate = dayjs(task.dueDate);
+    if(currentDate.isAfter(dueDate, 'day')) {
+        taskCard.css({'background-color':'#fd5c63', 'color': 'white'});
+        taskCardButton.addClass('btn-outline-light');
+    }
+    else if (currentDate.isSame(dueDate, 'day')) {
+        taskCard.css({'background-color':'#FFD700', 'color': 'white'})
+    }
+
+    //Makes the cards draggable
+    $('.draggable').draggable({
+        opacity: 0.7,
+        zIndex: 100,
+        
+        helper: function (e) {
+        
+            const original = $(e.target).hasClass('ui-draggable')
+            ? $(e.target)
+            : $(e.target).closest('.ui-draggable');
+        
+            return original.clone().css({
+                width: original.outerWidth(),
+            });
+        },
+    });
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    toDoListDiv.empty();
+    inProgressCardsDiv.empty();
+    doneCardsDiv.empty();
+    
+    let tasksList = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    tasks.forEach(function(task) {
+    tasksList.forEach(function(task) {
         createTaskCard(task);
     });
 
@@ -47,7 +88,7 @@ function renderTaskList() {
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
     event.preventDefault();
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
     const task = {
         id:generateTaskId(),
         title: $('#task-title').val(),
@@ -56,13 +97,25 @@ function handleAddTask(event){
         status: 'to-do',
     }
 
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    taskList.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-    
+    const id = $(this).attr('id');
+    console.log(id);
+
+    let taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    const newTaskList = taskList.filter(function(task) {
+        return task.id !== id
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(newTaskList));
+
+    renderTaskList();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
@@ -72,14 +125,18 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    $('#submit').on('click', handleAddTask);
+    renderTaskList();
 
-    $('#submit').on('click', createTaskCard);
+    $('#submit').on('click', handleAddTask);
 
     $('#task-due-date').datepicker({
         changeMonth: true,
         changeYear: true,
     });
 
+    $('.lane').droppable({
+        accept: '.draggable',
+        drop: handleDrop,
+    });
 
 });
